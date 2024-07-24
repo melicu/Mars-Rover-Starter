a2efceb06bb22a7cdb2d8af0646c2bc972623638
unknown <melcusumano@gmail.com>
2024-07-23 19:26:28 -0500

    Rover Test 8 Complete

--- rover.js
+++ rover.js
@@ -1,5 +1,17 @@
 class Rover {
-   // Write code here!
+   constructor(position) {
+      this.position = position;
+      this.mode = "NORMAL";
+      this.generatorWatts = 110;
+   }
+   receiveMessage(message) {
+      //takes parameter of a Message object//
+      //updates properties on the Rover based on the message//
+      //ex: MOVE sould update the position property//
+      //returns an ANONYMOUS OBJECT with at least two properties: original message, results (array of results of each command on the message object)//
+      return message;
+
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -6,8 +6,27 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 
-describe("Rover class", function() {
+describe("Rover class", function () {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    let constructorValuesCheck = new Rover(98382);
+    expect(constructorValuesCheck.position).toBe(98382);
+    expect(constructorValuesCheck.mode).toBe("NORMAL");
+    expect(constructorValuesCheck.generatorWatts).toBe(110);
+  });
+
+  it("response returned by receiveMessage contains the name of the message", function () {
+    let rover = new Rover(98382);
+    let response = rover.receiveMessage("Test message with two commands");
+    expect(response).toBe("Test message with two commands");
+  });
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message('Test message with two commands', commands);
+    let rover = new Rover(98382);
+    let response = rover.receiveMessage(message);
+    
+  });
 
 });
